e7f941bfffedccea0c6ced3206f79eb5f3cf6745
avinash pawar <30456983+avinash-startupwala@users.noreply.github.com>
2017-08-26 15:15:33 +0530

    Update sendmailtest.php

--- sendmailtest.php
+++ sendmailtest.php
@@ -1,4 +1,13 @@
 <?php
 require 'PHPMailerAutoload.php';
 $mail = new PHPMailer;
-?>
+$mail->setFrom('avinashpawar059@gmail.com', 'Avinash Pawar');
+$mail->addAddress('avinash.pawar@startupwala.com', 'My Friend');
+$mail->Subject  = 'First PHPMailer Message';
+$mail->Body     = 'Hi! This is my first e-mail sent through PHPMailer.';
+if(!$mail->send()) {
+  echo 'Message was not sent.';
+  echo 'Mailer error: ' . $mail->ErrorInfo;
+} else {
+  echo 'Message has been sent.';
+}
