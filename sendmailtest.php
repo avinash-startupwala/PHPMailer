a7230988b492ef7098d7f6cab236d621efda4f99
avinash pawar <30456983+avinash-startupwala@users.noreply.github.com>
2017-08-26 15:30:06 +0530

    Update sendmailtest.php

--- sendmailtest.php
+++ sendmailtest.php
@@ -5,6 +5,9 @@ $mail->setFrom('avinashpawar059@gmail.com', 'Avinash Pawar');
 $mail->addAddress('avinash.pawar@startupwala.com', 'My Friend');
 $mail->IsSMTP();
 $mail->Host = "smtp.gmail.com";
+$mail->SMTPAuth = true;
+$mail->Username = 'avinashpawar059@gmail.com';
+$mail->Password = 'Myyahoo321';
 $mail->Subject  = 'First PHPMailer Message';
 $mail->Body     = 'Hi! This is my first e-mail sent through PHPMailer.';
 if(!$mail->send()) {
